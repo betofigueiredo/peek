a043b0f60e7a5c2eee0cf83e397d5318f997c6ac
Beto Figueiredo <beto@2sidesof1.com>
2025-11-13 21:59:18 -0300

    feat: buttons to expand lines to show

--- vite/src/components/request-view-data.tsx
+++ vite/src/components/request-view-data.tsx
@@ -1,83 +1,112 @@
-import type { ReactElement } from "react";
+import { useEffect, useState, type ReactElement } from "react";
 import { useRequestStore } from "@/store";
 import { Button } from "@/components/ui/button";
+import { UnfoldVertical } from "lucide-react";
 
 export function RequestViewData({ id }: { id: string }) {
+  type Block = { start: number; end: number };
   const request = useRequestStore((state) => state.requests[id]);
   const select = useRequestStore((state) => state.select);
+  const [blocks, setBlocks] = useState<Block[]>([]);
+  const adjacentLinesToShow = 10;
 
-  // const linesToShow = request.responseAsArray.slice(0, 500);
+  const search = "luiz fernando";
 
   function clear() {
     select(null);
   }
 
-  function getAdjacentsLines(
-    start: number,
-    end: number,
-    search: string,
-  ): ReactElement[] {
-    const result = [];
-    for (let i = start; i < end; i++) {
-      const line = request.responseAsArray[i];
-      const highlight = line.toLowerCase().includes(search.toLowerCase())
-        ? "bg-amber-100 text-black"
-        : "";
-      result.push(
-        <div
-          key={i}
-          className={`py-1 truncate whitespace-pre-wrap font-mono text-base ${highlight}`}
-        >
-          <span className="opacity-30">{i}</span> {request.responseAsArray[i]}
-        </div>,
-      );
-    }
-    return result;
+  /**
+   * Button to expand and view more lines, up and down
+   */
+  function showMoreLines(idx: number) {
+    const updatedBlocks = blocks.map((block) => {
+      // TODO: check if match the previous block
+      if (block.start === idx) {
+        block.start -= adjacentLinesToShow;
+      }
+      // TODO: check if match the next block
+      if (block.end === idx) {
+        block.end += adjacentLinesToShow;
+      }
+      return block;
+    });
+    setBlocks(updatedBlocks);
   }
 
-  function findSearch(): ReactElement[] {
-    const search = "luiz fernando";
-    const adjacentLinesToShow = 20;
+  /**
+   * Find matchs on first render
+   */
+  useEffect(() => {
+    // TODO: without search show first 500
+
     const result = [];
     let idx = 0;
 
     while (idx < request.responseAsArray.length) {
-      const line = request.responseAsArray[idx];
-
-      if (line.toLowerCase().includes(search.toLowerCase())) {
-        const previousLines = getAdjacentsLines(
-          idx - adjacentLinesToShow,
-          idx,
-          search,
-        );
-        result.push(...previousLines);
-
-        result.push(
-          <div
-            key={idx}
-            className="py-1 truncate whitespace-pre-wrap font-mono text-base bg-amber-100 text-black"
-          >
-            <span className="opacity-30">{idx}</span> {line}
-          </div>,
-        );
-
-        const nextLines = getAdjacentsLines(
-          idx + 1,
-          idx + adjacentLinesToShow + 1,
-          search,
-        );
-        result.push(...nextLines);
-
-        result.push(<div key={`${idx}-space1`}> - </div>);
-        result.push(<div key={`${idx}-space2`}> - </div>);
-        result.push(<div key={`${idx}-space3`}> - </div>);
-
-        idx += 6;
+      const line = request.responseAsArray[idx].toLowerCase();
+      if (line.includes(search.toLowerCase())) {
+        result.push({
+          start: idx - adjacentLinesToShow,
+          end: idx + adjacentLinesToShow + 1,
+        });
+        idx += adjacentLinesToShow + 1;
       } else {
         idx += 1;
       }
     }
 
+    setBlocks(result);
+  }, [request.responseAsArray]);
+
+  function renderElements(): ReactElement[] {
+    const result = [];
+    let spaces = 0;
+    for (const block of blocks) {
+      // Button to expand previous lines
+      result.push(
+        <button
+          key={`${block.start}-unfold-previous`}
+          onClick={() => showMoreLines(block.start)}
+        >
+          <UnfoldVertical />
+        </button>,
+      );
+      // Render lines
+      for (let i = block.start; i < block.end; i++) {
+        const line = request.responseAsArray[i];
+        if (!line) {
+          continue;
+        }
+        const highlight = line.toLowerCase().includes(search.toLowerCase())
+          ? "bg-amber-100 text-black"
+          : "";
+        result.push(
+          <div
+            key={i}
+            className={`py-1 truncate whitespace-pre-wrap font-mono text-base ${highlight}`}
+          >
+            <span className="opacity-30">{i}</span> {request.responseAsArray[i]}
+          </div>,
+        );
+      }
+      // Button to expand next lines
+      result.push(
+        <button
+          key={`${block.end}-unfold-next`}
+          onClick={() => showMoreLines(block.end)}
+        >
+          <UnfoldVertical />
+        </button>,
+      );
+      result.push(
+        <div
+          key={`space-${spaces}`}
+          className="block h-0.5 bg-gray-700 mt-14 mb-16"
+        />,
+      );
+      spaces += 1;
+    }
     return result;
   }
 
@@ -87,7 +116,7 @@ export function RequestViewData({ id }: { id: string }) {
         <Button onClick={clear}>close</Button>
         <div>{request.id}</div>
         <div className="p-5 bg-(--secondary-background) border border-(--panel-border)">
-          {findSearch()}
+          {renderElements()}
         </div>
       </div>
       <div
